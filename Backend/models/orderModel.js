c1827f1ffaf6bd310dba2c771c5652eb0fb3e351
Ajinkya Bhilare <ajinkya0011@gmail.com>
2023-02-27 20:51:27 +0530

    MongoDBconnection_Established

--- Backend/models/orderModel.js
+++ Backend/models/orderModel.js
@@ -29,13 +29,41 @@ const orderSchema = mongoose.schema(
       type: String,
       required: true,
     },
-    paymentMethod: {
-      type: String,
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    shippingPrice: {
+      type: Number,
       required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
     },
   },
   { timestamps: true }
 );
 
-const user = mongoose.model("User", userSchema);
-export default User;
+const Order = mongoose.model("Order", userSchema);
+export default Order;
