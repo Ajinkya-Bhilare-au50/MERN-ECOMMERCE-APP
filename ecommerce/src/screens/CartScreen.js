8a3053466c67e5bf53626380ddcbb2a3113c6611
Ajinkya Bhilare <ajinkya0011@gmail.com>
2023-03-02 16:21:12 +0530

    Qty and Id Extracted from cart

--- ecommerce/src/screens/CartScreen.js
+++ ecommerce/src/screens/CartScreen.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import {
+  Link,
+  useLocation,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
+const CartScreen = ({ match }) => {
+  const search = useLocation().search;
+  const { id } = useParams();
 
-const CartScreen = () => {
+  const qty = new URLSearchParams(search).get("qty");
+
+  console.log("qty" + qty);
+  console.log("id" + id);
+
+  
+  const dispatch = useDispatch();
+    // const cart = useSelector(state => state.car)
+    // const {cartItems}
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, qty, id]);
+
   return (
     <div>
-      cart
+      <h1>Add to CART</h1>
     </div>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
